2b26798fde7b053f412f7b9eea6bd61c6933c9c7
Gavin Mogan <git@gavinmogan.com>
2019-10-04 19:24:37 -0700

    Updates

---
+++ precache-manifest.c03e60ff97656921d05e4b788dbf3cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cd619f247944fad46152215e2476b9",
+    "url": "/blueocean-ko-tester/index.html"
+  },
+  {
+    "revision": "4794872a8cb595567a56",
+    "url": "/blueocean-ko-tester/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "06c46e8e25b60b086a0d",
+    "url": "/blueocean-ko-tester/static/js/2.61060185.chunk.js"
+  },
+  {
+    "revision": "4794872a8cb595567a56",
+    "url": "/blueocean-ko-tester/static/js/main.5219a307.chunk.js"
+  },
+  {
+    "revision": "a4a9bf2e0af0badbe7ea",
+    "url": "/blueocean-ko-tester/static/js/runtime-main.5ce9cca7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d691999615e0419aaddcb06f67fd7fe.js"
+  "/blueocean-ko-tester/precache-manifest.c03e60ff97656921d05e4b788dbf3cd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blueocean-ko-tester/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
